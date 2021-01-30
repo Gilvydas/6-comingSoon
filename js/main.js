92b72a96a7d35b5dbd0d2525cf4eea355b17db3f
Gilvydas <gilvydas.kuprevicius@gmail.com>
2021-01-30 17:14:33 +0200

    Render clock #15 Clock design #12

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-import { socialsData } from './data/socialsData.js'
-import { renderSocials } from './components/renderSocials.js'
+import { socialsData } from './data/socialsData.js';
+import { renderSocials } from './components/renderSocials.js';
+import { clock } from './components/clock.js';
 
+clock('.clock');
 renderSocials('.social-row', socialsData);
