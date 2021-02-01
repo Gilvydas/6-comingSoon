a3b6932cf66083b153de9c3236583faab8d52a94
Gilvydas <gilvydas.kuprevicius@gmail.com>
2021-02-01 13:39:57 +0200

    ticking clock

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,7 +1,10 @@
 function clock(selector) {
-    const DOM = document.querySelector(selector);
+    const clockDOM = document.querySelector(selector);
+    let allValuesDOM = null;
+
+    const deadline = '01-04 14:00:00';
     const labels = ['days', 'hours', 'minutes', 'seconds']
-    const numbers = [432, 9, 37, 39]
+    let numbers = calctime(deadline);
     let HTML = '';
     for (let i = 0; i < 4; i++) {
 
@@ -11,7 +14,19 @@ function clock(selector) {
                 </div>`
     }
 
-    DOM.innerHTML = HTML;
+    clockDOM.innerHTML = HTML;
+    allValuesDOM = document.querySelectorAll(`${selector} .value`);
+
+
+
+
+    setInterval(function () {
+        numbers = calctime(deadline);
+        for (let i = 0; i < 4; i++) {
+            allValuesDOM[i].innerText = numberFormat(numbers[i])
+        }
+
+    }, 1000);
 
 }
 
@@ -20,7 +35,54 @@ function numberFormat(number) {
     if (number < 10) {
         return '0' + number;
     }
-    return number
+    return number;
 }
+
+
+
+function calctime(deadline) {
+    const date = new Date();
+    const now = Date.now();
+    let year = date.getFullYear();
+    let nextDeadline = `${year}-${deadline}`
+    let nextDeadlineInMiliseconds = (new Date(nextDeadline)).getTime();
+
+
+    if (nextDeadlineInMiliseconds < now) {
+        year++
+        nextDeadline = `${year}-${deadline}`
+        nextDeadlineInMiliseconds = (new Date(nextDeadline)).getTime();
+
+    }
+
+
+
+    // const timeDifference = nextDeadlineInMiliseconds - now;
+    // const seconds = Math.round(timeDifference / 1000);
+    // const minutes = Math.floor(seconds / 60);
+    // const hours = Math.floor(minutes / 60);
+    // const days = Math.floor(hours / 24);
+    // const years = Math.floor(days / 365);
+    // console.log(years);
+
+    const timeDifference = nextDeadlineInMiliseconds - now;
+    const seconds = Math.round(timeDifference / 1000);
+
+    const days = Math.floor(seconds / 60 / 60 / 24)
+    let unusedSeconds = seconds - days * 60 * 60 * 24;
+
+    const hours = Math.floor(unusedSeconds / 60 / 60);
+    unusedSeconds -= hours * 60 * 60;
+
+    const minutes = Math.floor(unusedSeconds / 60);
+    unusedSeconds -= minutes * 60
+
+
+
+
+    return [days, hours, minutes, unusedSeconds]
+};
+
+
 
 export { clock }
