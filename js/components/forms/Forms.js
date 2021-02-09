e979b7c758e7cc283181fbf1bac36e6018cc281d
Gilvydas <gilvydas.kuprevicius@gmail.com>
2021-02-09 12:33:50 +0200

    form inputs

---
+++ js/components/forms/Forms.js
@@ -0,0 +1,48 @@
+class Forms {
+    constructor() {
+        this.forms = [];
+
+        this.init();
+
+    }
+
+    init() {
+        this.findAllForms();
+        this.addEvents();
+
+
+    }
+
+    findAllForms() {
+        const forms = document.querySelectorAll('.form');
+        this.forms = [...forms];
+
+    }
+    addEvents() {
+        for (const form of this.forms) {
+            const inputs = form.querySelectorAll('input');
+            const textareas = form.querySelectorAll('textarea')
+            const allInputs = [...inputs, ...textareas]
+            const submit = form.querySelector('.btn');
+
+            submit.addEventListener('click', (event) => {
+                event.preventDefault();
+                console.log(allInputs);
+
+                for (const input of allInputs) {
+                    console.log(input.value);
+                };
+            });
+
+        }
+    }
+
+
+
+
+
+
+
+}
+
+export { Forms }
--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ import { progressBarData } from './data/progressBarData.js';
 import { ProgressBar } from './components/ProgressBar.js'
 import { socialsData } from './data/socialsData.js';
 import { renderSocials } from './components/renderSocials.js';
-
+import { Forms } from './components/forms/Forms.js'
 clock('.clock', '01-04 14:00:00');
 renderSocials('.social-row', socialsData);
-const progress = new ProgressBar('.left-column', progressBarData);
-console.log(progress);
-// progress.isValidSelector();
-// progress.render();
+new ProgressBar('.left-column', progressBarData);
+new Forms();
+
+
+
+
 
 
